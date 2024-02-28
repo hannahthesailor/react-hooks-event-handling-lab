22254126fd6ab41d04d6b5e0d50b59939ade31fb
Hannah Bradley <hannahthesailor@yahoo.com>
2024-02-28 02:23:22 -0500

    react-hooks-event-handling-lab.md

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
+import React from "react";
+
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(event) {
+        console.log("Entering password...");
+    }
     return (
-        <div></div>
+       
+        <div>
+             <input type="password"
+            onChange={handleChange} />
+
+        </div>
     )
 }
 
